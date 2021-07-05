5df47e31794d0c6cd00b01d291e6d28bae4c2f21
ZhekLu <justletmego@list.ru>
2021-07-05 20:40:57 +0300

    2G

---
+++ contest2/G.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+using namespace std; 
+
+void sortSide(vector<int>& arr, int lenSide);
+int* maxMulti(vector<int>& arr);
+
+int main()
+{
+    ifstream fin("input.txt");
+    if(fin.is_open())
+    {
+        vector<int> sp;
+        int num = 0;
+        while(fin >> num)
+            sp.push_back(num);
+        fin.close();
+        sortSide(sp, 2);
+        int *ans = maxMulti(sp);
+        for(int i = 0; i < 2; i++)
+            cout << ans[i] << " ";
+    }
+}
+
+void sortSide(vector<int>& arr, int lenSide)
+{
+    for(int i = 0; i < lenSide; i++)
+    {
+        for(int k = i, j = arr.size() - 1 - i; k < arr.size() - 1 - i && j > 0 + i; k++, j--)
+        {
+            int temp;
+            if(arr[k] > arr[k + 1])
+            {
+                temp = arr[k];
+                arr[k] = arr[k + 1];
+                arr[k + 1] = temp;
+            }
+            if(arr[j] < arr[j - 1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = temp;
+            }
+        }
+    }
+}
+
+int* maxMulti(vector<int>& arr)
+{
+    int* ans = new int[2]{0};
+    if((long long)arr[arr.size() - 1] * (long long)arr[arr.size() - 2] > (long long)arr[0] * (long long)arr[1])
+    {    
+        ans[0] = arr[arr.size() - 2];
+        ans[1] = arr[arr.size() - 1];
+    }
+    else  
+    {
+        ans[0] = arr[0];
+        ans[1] = arr[1];
+    }
+    return ans; 
+}
