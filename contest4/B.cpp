98311459fc0043f20a80995f02ec8887ef5075d4
ZhekLu <justletmego@list.ru>
2021-07-09 13:49:09 +0300

    3B

---
+++ contest4/B.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <unordered_map>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    ifstream fin("input.txt");
+    if(fin.is_open())
+    {
+        unordered_map<string, int> bookMap;
+        string word;
+        while(fin >> word)
+        {
+            cout << bookMap[word] << " ";
+            bookMap[word]++;
+        }
+    }
+}
