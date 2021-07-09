c4d8ec5ef852778fa3b31763e8ba0370b057f894
ZhekLu <justletmego@list.ru>
2021-07-09 14:09:33 +0300

    4D

--- contest4/D.cpp
+++ contest4/D.cpp
@@ -1,8 +1,39 @@
-#include <map>
+#include <unordered_map>
 #include <iostream>
 #include <string>
 using namespace std; 
 
+int main()
+{
+    int quantity;
+    cin >> quantity;
+    unordered_map<int, int> keys(quantity);
+    for(int i = 1; i <= quantity; i++)
+    {
+        int maxQ;
+        cin >> maxQ;
+        keys[i] = maxQ;
+    }
+
+    int pushQ;
+    cin >> pushQ;
+    for(int i = 0; i < pushQ; i++)
+    {
+        int key;
+        cin >> key;
+        keys[key]--;
+    }
+
+    for(int k = 1; k <= quantity; k++)
+    {
+        if(keys[k] < 0)
+            cout << "YES\n";
+        else  
+            cout << "NO\n";
+    }
+}
+
+/*
 int main()
 {
     int quantity;
@@ -32,34 +63,4 @@ int main()
             cout << "NO\n";
     }
 }
-
-// //with anordered_map
-// int main()
-// {
-//     int quantity;
-//     cin >> quantity;
-//     unordered_map<int, int> keys(quantity);
-//     for(int i = 1; i <= quantity; i++)
-//     {
-//         int maxQ;
-//         cin >> maxQ;
-//         keys[i] = maxQ;
-//     }
-
-//     int pushQ;
-//     cin >> pushQ;
-//     for(int i = 0; i < pushQ; i++)
-//     {
-//         int key;
-//         cin >> key;
-//         keys[key]--;
-//     }
-
-//     for(int k = 1; k <= quantity; k++)
-//     {
-//         if(keys[k] < 0)
-//             cout << "YES\n";
-//         else  
-//             cout << "NO\n";
-//     }
-// }
+*/
