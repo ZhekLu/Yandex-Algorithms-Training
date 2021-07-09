706eadc91133a3436b4d2ecba3e4a7dfe188ee7e
ZhekLu <justletmego@list.ru>
2021-07-09 14:08:05 +0300

    4D_map

--- contest4/D.cpp
+++ contest4/D.cpp
@@ -1,14 +1,13 @@
-#include <unordered_map>
+#include <map>
 #include <iostream>
 #include <string>
 using namespace std; 
 
-//with anordered_map
 int main()
 {
     int quantity;
     cin >> quantity;
-    unordered_map<int, int> keys(quantity);
+    map<int, int> keys;
     for(int i = 1; i <= quantity; i++)
     {
         int maxQ;
@@ -25,11 +24,42 @@ int main()
         keys[key]--;
     }
 
-    for(int k = 1; k <= quantity; k++)
+    for(auto& key : keys)
     {
-        if(keys[k] < 0)
+        if(key.second < 0)
             cout << "YES\n";
         else  
             cout << "NO\n";
     }
 }
+
+// //with anordered_map
+// int main()
+// {
+//     int quantity;
+//     cin >> quantity;
+//     unordered_map<int, int> keys(quantity);
+//     for(int i = 1; i <= quantity; i++)
+//     {
+//         int maxQ;
+//         cin >> maxQ;
+//         keys[i] = maxQ;
+//     }
+
+//     int pushQ;
+//     cin >> pushQ;
+//     for(int i = 0; i < pushQ; i++)
+//     {
+//         int key;
+//         cin >> key;
+//         keys[key]--;
+//     }
+
+//     for(int k = 1; k <= quantity; k++)
+//     {
+//         if(keys[k] < 0)
+//             cout << "YES\n";
+//         else  
+//             cout << "NO\n";
+//     }
+// }
