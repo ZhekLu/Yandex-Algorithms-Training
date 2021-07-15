eaf490ca90ae1bae840197247c736e88981a7411
ZhekLu <justletmego@list.ru>
2021-07-15 19:28:40 +0300

    6D optimized

--- contest6/D.cpp
+++ contest6/D.cpp
@@ -3,7 +3,9 @@ using namespace std;
 
 inline bool checkSize(long long protecSize, const long long* params)
 {
-    return (params[3] / (params[1] + 2 * protecSize)) * (params[4] / (params[2] + 2 * protecSize)) >= params[0];
+    long long horizontalLocation = (params[3] / (params[1] + 2 * protecSize)) * (params[4] / (params[2] + 2 * protecSize));  
+    long long verticalLocation = (params[3] / (params[2] + 2 * protecSize)) * (params[4] / (params[1] + 2 * protecSize));  
+    return (verticalLocation > horizontalLocation ? verticalLocation : horizontalLocation) >= params[0];
 }
 
 long long RBinSearch(long long l, long long r, bool(*check)(long long, const long long*),const long long* checkParams)
@@ -26,12 +28,7 @@ int main()
     long long dimentions[5];
     for(int i = 0; i < 5; i++)
         cin >> dimentions[i]; 
-    // a > | < b 
-    long long horLocation =  RBinSearch(0, dimentions[3] - dimentions[1], checkSize, dimentions);
-    // a < | > b
-    swap(dimentions[1], dimentions[2]);
-    long long verLocation =  RBinSearch(0, dimentions[3] - dimentions[1], checkSize, dimentions);
 
-    cout << (horLocation > verLocation ? horLocation : verLocation);
+    cout << RBinSearch(0, dimentions[3] - (dimentions[1] < dimentions[2] ? dimentions[1] : dimentions[2]), checkSize, dimentions);
     return 0;
 }
