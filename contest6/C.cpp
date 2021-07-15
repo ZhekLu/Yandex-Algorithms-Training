b0e7c1b16653a172836267cab50303bf4eccc554
ZhekLu <justletmego@list.ru>
2021-07-15 18:03:35 +0300

    6C

--- contest6/C.cpp
+++ contest6/C.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 using namespace std;
 
-bool checkSize(unsigned sideLen, const unsigned* params)
+inline bool checkSize(long long sideLen, const unsigned* params)
 {
     return (sideLen / params[0]) * (sideLen / params[1]) >= params[2];
 }
 
-unsigned LBinSearch(unsigned l, unsigned r, bool(*check)(unsigned, const unsigned*),const unsigned* checkParams)
+long long LBinSearch(long long l, long long r, bool(*check)(long long, const unsigned*),const unsigned* checkParams)
 {
-    unsigned m;
+    long long m;
     while(l < r)
     {
         m = (l + r) / 2;
@@ -25,5 +25,5 @@ int main()
     unsigned input[3]; //w, h, n;
     for(int i = 0; i < 3; i++)
         cin >> input[i];
-    cout << LBinSearch(0, input[2] * (input[0] > input[1] ? input[0] : input[1]), checkSize, input);
+    cout << LBinSearch(0, (long long)(input[2] + 2) / 2 * (input[0] > input[1] ? input[0] : input[1]), checkSize, input);
 }
