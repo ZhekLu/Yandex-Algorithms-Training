e13309cf588df4f7755890f9a0ab978e7c0ff491
ZhekLu <justletmego@list.ru>
2021-08-04 18:28:48 +0300

    Upt tree Class

--- contest8/tree.cpp
+++ contest8/tree.cpp
@@ -1,3 +1,5 @@
+#include <vector>
+#include <iostream>
 class Tree
 {
 public:
@@ -9,13 +11,17 @@ public:
     void remove(int key);
     void clear();
     int getHeight() const;
+    bool isBalanced() const;
+    int getMax(int num);
+    const std::vector<int>& getDepths() const; 
 private:
     // node struct
     struct Node
     {
-        explicit Node(int value) : key(value) {}
+        explicit Node(int value, int depth = 0) : key(value), depth(depth) {}
         int key;
         int height = 0;
+        int depth;
         Node* left = nullptr;
         Node* right = nullptr;
         void UpdateHeight()
@@ -24,9 +30,24 @@ private:
             int hright = (this->right) ? this->right->height : -1;
             this->height = (hleft > hright ? hleft : hright) + 1;
         }
+        static int UpdateDepths(bool isNew = 0)
+        {
+            static int depth = 0;
+            if(isNew)
+                depth = 0;
+            else  
+                depth++;
+            return depth;
+        }
+        
+        int balanceFactor();
+        bool TraversePostorderBalance(bool balance = true);
+        void TraverseInorder(int num, int* answ = nullptr);
+        void TraverseInorder();
     };
     // elements
     Node* root = nullptr;
+    std::vector<int> depths;
 
     // func methods
     void insert(int key, Node* currRoot);
@@ -42,9 +63,15 @@ Tree::~Tree()
 void Tree::insert(int key)
 {
     if (!root)
+    {
         root = new Node(key);
+        depths.push_back(1);
+    }
     else
+    {
         insert(key, root);
+        Node::UpdateDepths(true);
+    }
 }
 
 void Tree::remove(int key)
@@ -57,27 +84,101 @@ void Tree::clear()
     clear(root);
     root = nullptr;
 }
+const std::vector<int>& Tree::getDepths() const
+{
+    return depths; 
+}
 
 int Tree::getHeight() const
 {
     return root->height;
 }
 
+bool Tree::isBalanced() const
+{
+    return root->TraversePostorderBalance();
+}
+
+int Tree::getMax(int num)
+{
+    if(!root)
+        return INT32_MIN;
+    int answ;
+    root->TraverseInorder(num, &answ);
+
+    root->TraverseInorder(-1);
+    return answ;
+}
+
+void Tree::Node::TraverseInorder(int num, int* answ)
+{
+    static int count = 0;
+    if(num == -1)
+    {
+        count = 0;
+        return;
+    }
+
+    if(this->right)
+        this->right->TraverseInorder(num, answ);
+
+    count++;
+    if(count == num)
+        *answ = this->key;
+
+    if(this->left)
+        this->left->TraverseInorder(num, answ);
+
+}
+
+void Tree::Node::TraverseInorder()
+{
+
+    if(this->left)
+        this->left->TraverseInorder();
+
+    std::cout << this->key << std::endl;
+
+    if(this->right)
+        this->right->TraverseInorder();
+
+}
+
+bool Tree::Node::TraversePostorderBalance(bool balance)
+{
+    if(!balance)
+        return balance;
+    if(this->left)
+        balance = this->left->TraversePostorderBalance();
+    if(balance && this->right)
+        balance = this->right->TraversePostorderBalance();
+    if(balance)
+        balance = abs(this->balanceFactor()) < 2;
+    return balance;
+}
+
 void Tree::insert(int key, Node* currRoot)
 {
+    Node::UpdateDepths();
     if (currRoot->key > key)
     {
         if (currRoot->left)
             insert(key, currRoot->left);
-        else
-            currRoot->left = new Node(key);
+       else
+        {
+            currRoot->left = new Node(key, Node::UpdateDepths());
+            depths.push_back(currRoot->left->depth);
+        }
     }
     else if (currRoot->key < key)
     {
         if (currRoot->right)
             insert(key, currRoot->right);
         else
-            currRoot->right = new Node(key);
+        {
+            currRoot->right = new Node(key, Node::UpdateDepths());
+            depths.push_back(currRoot->right->depth);
+        }
     }
     currRoot->UpdateHeight();
 }
@@ -122,7 +223,15 @@ Tree::Node* Tree::remove(int key, Node* currRoot)
     
     if (currRoot)
         currRoot->UpdateHeight();
+    
     return currRoot;
+}
+
+int Tree::Node::balanceFactor()
+{
+    int hleft = (this->left) ? this->left->height : -1;
+    int hright = (this->right) ? this->right->height : -1;
+    return hright - hleft;
 }
 
 void Tree::clear(Node* currRoot)
