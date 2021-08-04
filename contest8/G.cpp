8d1212fc22400ab5ee88864b291fce4946e407cb
ZhekLu <justletmego@list.ru>
2021-08-04 16:16:06 +0300

    8G

---
+++ contest8/G.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+using namespace std;
+class Tree
+{
+public:
+    // ctor & destr
+    ~Tree();
+   
+    // methods
+    void insert(int key);
+    void clear();
+    void TraverseInorderBranch();
+private:
+    // node struct
+    struct Node
+    {
+        explicit Node(int value) : key(value) {}
+        int key;
+        Node* left = nullptr;
+        Node* right = nullptr;
+        void TraverseInorderBranch();
+    };
+    // elements
+    Node* root = nullptr;
+
+    // func methods
+    void insert(int key, Node* currRoot);
+    void clear(Node* currRoot);
+};
+
+Tree::~Tree()
+{
+    this->clear();
+}
+
+void Tree::insert(int key)
+{
+    if (!root)
+        root = new Node(key);
+    else
+        insert(key, root);
+}
+
+void Tree::clear()
+{
+    clear(root);
+    root = nullptr;
+}
+
+void Tree::TraverseInorderBranch()
+{
+    root->TraverseInorderBranch();
+}
+
+void Tree::Node::TraverseInorderBranch()
+{
+
+    if(this->left)
+        this->left->TraverseInorderBranch();
+
+    if(this->left && !this->right || !this->left && this->right)
+        cout << this->key << endl;
+
+    if(this->right)
+        this->right->TraverseInorderBranch();
+
+}
+
+void Tree::insert(int key, Node* currRoot)
+{
+    if (currRoot->key > key)
+    {
+        if (currRoot->left)
+            insert(key, currRoot->left);
+        else
+            currRoot->left = new Node(key);
+    }
+    else if (currRoot->key < key)
+    {
+        if (currRoot->right)
+            insert(key, currRoot->right);
+        else
+            currRoot->right = new Node(key);
+    }
+}
+
+void Tree::clear(Node* currRoot)
+{
+    if (!currRoot)
+        return;
+    if (currRoot->left)
+        clear(currRoot->left);
+    if (currRoot->right)
+        clear(currRoot->right);
+    delete currRoot;
+}
+
+
+int main()
+{
+    Tree tree;
+    int value;
+    cin >> value;
+    while(value)
+    {
+        tree.insert(value);
+        cin >> value;
+    }
+    tree.TraverseInorderBranch();
+}
